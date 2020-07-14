0d8315488656bada19cfda3a2b190ca6af8a5a8d
Pfeszler <pfeszler@gmail.com>
2020-07-14 14:40:48 +0200

    Updates

---
+++ precache-manifest.907ba5942f77390ba8e29e3679571477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f96d8ca41482c0b68df324063fb9be",
+    "url": "/To-do-list-React/index.html"
+  },
+  {
+    "revision": "fc42af0f2d7104237511",
+    "url": "/To-do-list-React/static/css/main.78386d70.chunk.css"
+  },
+  {
+    "revision": "a99f3f11606fd2c20522",
+    "url": "/To-do-list-React/static/js/2.25beb3f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list-React/static/js/2.25beb3f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc42af0f2d7104237511",
+    "url": "/To-do-list-React/static/js/main.a3cb2490.chunk.js"
+  },
+  {
+    "revision": "a89071f69dfd0d799ec8",
+    "url": "/To-do-list-React/static/js/runtime-main.6b0a247e.js"
+  }
+]);
