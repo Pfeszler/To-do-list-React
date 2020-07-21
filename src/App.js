8bfa58d9436f78365b8f2064c549e2415fbb9561
Pfeszler <pfeszler@gmail.com>
2020-07-21 11:00:00 +0200

    mistake removed

--- src/App.js
+++ src/App.js
@@ -6,17 +6,6 @@ import Form from "./Form"
 import Buttons from "./Buttons"
 import Tasks from "./Tasks"
 
-const currencies = [
-    { id: 1, name: "dolar amerykański", symbol: "USD", buyprice: 3.9384, sellprice: 4.0180 },
-    { id: 2, name: "euro", symbol: "EUR", buyprice: 4.4189, sellprice: 4.5081 },
-    { id: 3, name: "funt brytyjski", symbol: "GPB", buyprice: 4.9386, sellprice: 5.0384 },
-    { id: 4, name: "frank szwajcarski", symbol: "CHF", buyprice: 4.1437, sellprice: 4.2275 },
-    { id: 5, name: "korona norweska", symbol: "NOK", buyprice: 0.4113, sellprice: 0.4197 },
-    { id: 6, name: "korona czeska", symbol: "CZK", buyprice: 0.1660, sellprice: 0.1694 },
-]
-
-
-
 
 
 
