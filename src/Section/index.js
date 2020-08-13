65ff464fab0d495dd95faa078a5f712173167b2f
Pfeszler <pfeszler@gmail.com>
2020-08-13 23:07:10 +0200

    formating fix

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {StyledSection, HeaderContainer, Header} from "./styled"
+import { StyledSection, HeaderContainer, Header } from "./styled"
 
 
 const Section = ({ title, body, extraHeaderContent }) => (
