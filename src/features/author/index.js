8bfe33d901056206d6420525c80674c79f736f82
Pfeszler <pfeszler@gmail.com>
2020-09-20 19:44:32 +0200

    author text added

--- src/features/author/index.js
+++ src/features/author/index.js
@@ -4,16 +4,25 @@ import Header from "../../common/Header"
 import Section from "../../common/Section"
 
 const Author = () =>
-<Container>
-<Header
-    title="O Autorze" />
-<Section
-    title="Paweł Feszler"
-    body={
-        <div>
-            blbabla
-        </div>
-    }
-/>
-</Container>
+    <Container>
+        <Header
+            title="O Autorze" />
+        <Section
+            title="Paweł Feszler"
+            body={
+                <section>
+                    <p>
+                        Od początku tego roku poświęcam również czas na naukę programowania.
+                        Mój główny zakres zainteresowań w tej kwestii to technologie front-endowe ze szczególnym wskazaniem na javascript es6 oraz react.
+                        Obecnie uczestniczę w kursie "Frontend developer od podstaw" na https://youcode.pl/ oraz uczę się samodzielnie
+                    </p>
+                    <p>
+                        Link do profilu github:<br />
+                        https://github.com/Pfeszler/
+                    </p>
+                    <p>Od lat, z sukcesami, pracuję w sprzedaży. Największe doświadczenie mam w branży telekomunikacyjnej.</p>
+                </section>
+            }
+        />
+    </Container>
 export default Author
